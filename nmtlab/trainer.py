8677e439cbd68ccea3b0129eb582cefd3af56d68
shu <raphael@uaca.com>
2018-06-23 06:45:33 +0000

    -

--- nmtlab/trainer.py
+++ nmtlab/trainer.py
@@ -5,5 +5,25 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
+
 class MTTrainer(object):
+    
+    def __init__(self, model, dataset, optimizer, scheduler=None, multigpu=False):
+        self._model = model
+        self._dataset = dataset
+        self._optimizer = optimizer
+        self._scheduler = scheduler
+        self._multigpu = multigpu
+        # Setup horovod
+        if multigpu:
+            import horovod.torch as hvd
+            # Initialize Horovod
+            hvd.init()
+            # Pin GPU to be used to process local rank (one GPU per process)
+            torch.cuda.set_device(hvd.local_rank())
+        # Move model to GPU
+        self._model.cuda()
+    
+        
     
