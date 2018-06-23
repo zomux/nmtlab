2c40c2fd63914729c409123df133ac3494ba6a7d
shu <raphael@uaca.com>
2018-06-23 06:16:10 +0000

    -

--- nmtlab/utils/dataset.py
+++ nmtlab/utils/dataset.py
@@ -12,10 +12,14 @@ class MTDataset(object):
     """
     
     def __init__(self, corpus_path, src_vocab_path, tgt_vocab_path, max_length=60):
+        self._max_length = max_length
         self.train_data = torchtext.data.TabularDataset(
             path=corpus_path, format='tsv',
             fields=[('src', src), ('tgt', tgt)],
-            filter_pred=len_filter
+            filter_pred=self._len_filter
         )
+        
+    def _len_filter(self, sample):
+        return len(sample.src) <= 60 and len(sample.tgt) <= 60
     
 
