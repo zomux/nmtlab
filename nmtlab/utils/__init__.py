e9fd117241492e68e6cebe4e909b7b38e89c50b3
shu <raphael@uaca.com>
2018-06-23 06:14:05 +0000

    -

--- nmtlab/utils/__init__.py
+++ nmtlab/utils/__init__.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import, print_function, division
-from six.moves import xrange, zip
 
 from .mapdict import MapDict
 from .lazydict import LazyDict
+from .vocab import Vocab
+from .dataset import MTDataset
