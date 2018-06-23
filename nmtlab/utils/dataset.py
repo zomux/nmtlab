393810b0e88705dd8797cc576c21bbcc232db189
shu <raphael@uaca.com>
2018-06-23 06:20:30 +0000

    -

--- nmtlab/utils/dataset.py
+++ nmtlab/utils/dataset.py
@@ -22,13 +22,21 @@ class MTDataset(object):
         tgt = torchtext.data.Field(pad_token="<null>", preprocessing=lambda seq: ["<s>"] + seq + ["</s>"])
         tgt.vocab = Vocab(tgt_vocab_path)
         
-        self.train_data = torchtext.data.TabularDataset(
+        self._data = torchtext.data.TabularDataset(
             path=corpus_path, format='tsv',
             fields=[('src', src), ('tgt', tgt)],
             filter_pred=self._len_filter
         )
         
-    def _len_filter(self, sample):
+    @staticmethod
+    def _len_filter(sample):
         return len(sample.src) <= 60 and len(sample.tgt) <= 60
     
+    def __iter__(self):
+        batch_iterator = torchtext.data.BucketIterator(
+            dataset=self._data, batch_size=self._batch_size,
+            sort=False, sort_within_batch=True,
+            sort_key=lambda x: len(x.src),
+            device=None, repeat=False)
+        return iter(batch_iterator)
 
